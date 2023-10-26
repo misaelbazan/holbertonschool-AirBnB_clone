0d4c3b49ee8277bcec2e3f929d7820ba2f278218
misaelbfranco@gmail.com <misaelbfranco@gmail.com>
2023-10-26 16:45:26 -0700

    Required files uploaded

---
+++ models/base_model.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+This module contains 01 Classes:
+    1. BaseModel
+"""
+
+
+from uuid import uuid4
+from datetime import datetime, timezone
+
+class BaseModel:
+    """
+    This class is meant to inherit to the following classes:
+        -
+        -
+    This class contains:
+        Public instance attributes:
+        - id: string - assign with an uuid when an instance is created
+            + The goal is to have unique 'id' for each BaseModel
+        - created_at: datetime - assign with the current datetime when an \
+                instance is created
+        - updated_at: datetime - assign with the current datetime when an \
+                instance is created and it will be updated e/t you change your object
+        Methods:
+        - __str__: should print '[<class name>] (<self.id>) <self.__dict__>'
+        Public instance methods:
+        - save(self): updates the public instance attribute updated_at with the \
+                current datetime
+        - to_dict(self): returns a dictionary containing all keys/values of \
+                __dict__ of the instance
+    """
+
+    def __init__(self):
+        """Initialize a new instance from BaseModel Class
+        Variables:
+        - id: string - assign with an uuid when an instance is created:
+        - created_at: datetime - assign with the current datetime when an instance\
+                is created
+        - updated_at: datetime - assign with the current datetime when an instance\
+                is created and it will be updated e/t you change your object
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns [class name] (self.id) self.__dict__"""
+        return(f"{[__class__.__name__]} ({self.id}) {self.__dict__}")
+
+    def save(self):
+        """Updates the 'updated_at' attribute with the current d/t"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ \
+                of the instance"""
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+        self.__dict__["__class__"] = __class__.__name__
+        return(self.__dict__)
