6be8c6837f202a864134846e096d8eae4907dc31
MayraVasCo <mayravas2808@gmail.com>
2023-10-26 20:59:10 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,24 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 
 import uuid
 from datetime import datetime
+
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-        def __str__(self):
-            class_name = self.__class__.__name
-            return f"[{class_name}] ({self.id}) {self.__dict__}"
+    def __str__(self):
+        class_name = self.__class__.__name
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
 
-        def save(self):
-            self.updated_at = datetime.now()
+    def save(self):
+        self.updated_at = datetime.now()
             
-        def to_dict(self):
-            instance_dict = self.__dict__.copy()
-            instance_dict['created_at'] = self.created_at.isoformat()
-            instance_dict['updated_at'] = self.updated_at.isoformat()
-            instance_dict['__class__'] = self.__class__.__name
-            return instance_dict
+    def to_dict(self):
+        instance_dict = self.__dict__.copy()
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
+        instance_dict['__class__'] = self.__class__.__name
+        return instance_dict
