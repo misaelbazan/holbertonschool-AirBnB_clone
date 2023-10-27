60beee01f14d8cda0eaa09c56c29fa45b97e551b
MayraVasCo <mayravas2808@gmail.com>
2023-10-26 20:40:21 -0700

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,4 +1,13 @@
 import unittest
-from models.base_model import BaseModel
+from models/base_model import BaseModel
 
+class BaseModelTestCase(unittest.TestCase):
+    def test_constructor(self):
+        pass
+    def test_save(self):
+        pass
+    def test_to_dict(self):
+        pass
 
+if __name__ == '__main__':
+    unittest.main()
