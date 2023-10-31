1b9b996c21429ed68bdc8f32a9fe1119f13424b2
misaelbfranco@gmail.com <misaelbfranco@gmail.com>
2023-10-30 18:17:43 -0700

    File: base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """
 This module contains 01 classes:
     - BaseModel
 """
 
-=======
->>>>>>> 6be8c6837f202a864134846e096d8eae4907dc31
-
 import uuid
 from datetime import datetime
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 6be8c6837f202a864134846e096d8eae4907dc31
 class BaseModel:
     """This class creates an object from scratch
     """
@@ -31,31 +24,20 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def __str__(self):
-<<<<<<< HEAD
         """Returns a string in this format:
             <class name> (self.id) self.__dict__
         """
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """This method updates the updated_at """ 
+        """This method updates the updated_at """
         self.updated_at = datetime.now()
 
-=======
-        class_name = self.__class__.__name
-        return f"[{class_name}] ({self.id}) {self.__dict__}"
-
-    def save(self):
-        self.updated_at = datetime.now()
-            
->>>>>>> 6be8c6837f202a864134846e096d8eae4907dc31
     def to_dict(self):
+        """This method retuns a copy of an instance dictionary"""
         instance_dict = self.__dict__.copy()
         instance_dict['created_at'] = self.created_at.isoformat()
         instance_dict['updated_at'] = self.updated_at.isoformat()
-<<<<<<< HEAD
         instance_dict['__class__'] = self.__class__.__name__
-=======
-        instance_dict['__class__'] = self.__class__.__name
->>>>>>> 6be8c6837f202a864134846e096d8eae4907dc31
+        instance_dict['__class__'] = self.__class__.__name__
         return instance_dict
