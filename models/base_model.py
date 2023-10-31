f5036de7105140f16a7da8397a89bcdb2a752583
misaelbfranco@gmail.com <misaelbfranco@gmail.com>
2023-10-30 19:14:30 -0700

    File:base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,20 +12,25 @@ class BaseModel:
     """This class creates an object from scratch
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize an BaseModel instance
         Args:
+            *args: Any positional arguments.
+            **kwargs: Any keyword arguments.
             id - a string, id - unique assign
             created_at - datetime assign with the current time of creation
             updated_at - datetime with the current time of updating
         """
+        for key, value in kwargs.items():
+            if key != '__class__':
+                setattr(self, key, value)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string in this format:
-            <class name> (self.id) self.__dict__
+            BaseModel (uuid4 type id) self.__dict__
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
@@ -38,6 +43,5 @@ class BaseModel:
         instance_dict = self.__dict__.copy()
         instance_dict['created_at'] = self.created_at.isoformat()
         instance_dict['updated_at'] = self.updated_at.isoformat()
-        instance_dict['__class__'] = self.__class__.__name__
         instance_dict['__class__'] = self.__class__.__name__
         return instance_dict
