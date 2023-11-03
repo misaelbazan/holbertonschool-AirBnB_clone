a56033d7e1f40c648ea1a94de48383d142a509a0
misaelbfranco@gmail.com <misaelbfranco@gmail.com>
2023-11-02 20:08:13 -0700

    commited the last changes

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,12 @@ class BaseModel:
         if bool(kwargs):
             for k, v in kwargs.items():
                 if k != '__class__':
-                    setattr(self, key, value)
+                    setattr(self, k, v)
+
+            self.__dict__["created_at"] = datetime.strptime(
+                    self.__dict__["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            self.__dict__["updated_at"] = datetime.strptime(
+                    self.__dict__["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
